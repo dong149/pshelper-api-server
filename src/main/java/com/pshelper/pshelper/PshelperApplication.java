00448a976ac45f422bda96181af7e1ff0abda71b
DANNIEL <donghoon149@gmail.com>
2021-05-03 20:01:04 +0900

    Initial commit

---
+++ src/main/java/com/pshelper/pshelper/PshelperApplication.java
@@ -0,0 +1,13 @@
+package com.pshelper.pshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PshelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PshelperApplication.class, args);
+    }
+
+}
